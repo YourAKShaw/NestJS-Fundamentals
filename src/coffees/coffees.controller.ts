cd935c751d44ef56ae283ea5c0cb80c1aad354ea
YourAKShaw <hitme@yourakshaw.com>
2023-06-11 16:00:12 +0530

    feat: :sparkles: add example of ApiForbiddenResponse decorator

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -13,11 +13,13 @@ import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Public } from 'src/common/decorators/public.decorator';
+import { ApiForbiddenResponse } from '@nestjs/swagger';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
   @Public()
   @Get()
   findAll(@Query() paginationQuery: PaginationQueryDto) {
